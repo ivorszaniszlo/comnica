115fa2161b6c55ff80adbfe915eee0a5fec9eb07
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-10-23 14:55:38 +0200

    fix: postcss.config and vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcssConfig from './postcss.config.cjs';
+import postcssConfig from './postcss.config.js';
 
 export default defineConfig({
   plugins: [react()],
