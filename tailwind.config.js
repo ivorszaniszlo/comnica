d8713a38acaf0b1f98b5e2c1fd5e8fde23b662d7
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2024-10-24 20:59:21 +0200

    refactor: remove MUI from project, update dependencies, and refactor code with English comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        customGray: '#31363c', // Egyedi sötétszürke szín
-        customPurple: '#682c5d', // Egyedi lila szín
-        customTealLight: '#29a98b', // Egyedi világos teal
-        customTeal: '#20876f', // Egyedi teal
-        customTealDark: '#186553', // Egyedi sötét teal
-        customGreenBGDark: '#C4E7DF', // Sötétebb zöld háttér
+        customGray: '#31363c', // Custom dark gray color
+        customPurple: '#682c5d', // Custom purple color
+        customTealLight: '#29a98b', // Custom light teal color
+        customTeal: '#20876f', // Custom teal color
+        customTealDark: '#186553', // Custom dark teal color
+        customGreenBGDark: '#C4E7DF', // Darker green background color
         customGreen: {
           DEFAULT: '#D4EDE7',
           dark: '#BFE4DB',
           darker: '#88CAB2',
         },
-        customPrimary: '#29a98b', // Az eredeti primary szín
-        customSecondary: '#682c5d', // Az eredeti secondary szín
+        customPrimary: '#29a98b', // Original primary color
+        customSecondary: '#682c5d', // Original secondary color
       },
     },
   },
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import postcssConfig from './postcss.config.js';
